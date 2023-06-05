f45164032b48422014b3a1b6fa72b851f77ced6d
Dhanraj-Subramani <121480921+Dhanraj-Subramani@users.noreply.github.com>
2023-06-05 20:18:08 +0530

    Add files via upload

---
+++ staff/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				VSBEC Leave System <a href="https://dhanraj-subramani.github.io/Portfolio/" target="_blank"><span>developed by </span> IT DEPARTMENT</a>
+			</div>
